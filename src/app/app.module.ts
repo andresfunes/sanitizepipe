bea37414a603d54199ab3e88289fb8312591ede3
Andres Funes <afunes@stacktrace.com.ar>
2017-09-14 16:45:52 -0300

    SafeHtmlPipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SafeHtmlPipe } from './safe.html.pipe';
+import { Page1Component } from './page1.component';
+import { Page2Component } from './page2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Page1Component,
+    Page2Component,
+    SafeHtmlPipe
   ],
   imports: [
     BrowserModule
